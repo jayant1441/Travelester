038cfede17ecbfd330ba14451ecc027b1e2b6f87
Raghav Dhingra <raghav.dhingra15@gmail.com>
2021-10-01 16:17:20 +0530

    Update router.jsx

--- src/router.jsx
+++ src/router.jsx
@@ -1,19 +1,18 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import MetroDashboard from "./MetroDashboard";
 import "./assets/common/default.css";
 import "./assets/common/loader.css";
 import "./assets/common/helper.css";
-import MetroDashboard from "./MetroDashboard";
 
 class Routes extends Component {
   state = {};
+  
   render() {
     return (
-      <>
         <Router>
           <Route exact path="/" component={MetroDashboard} />
         </Router>
-      </>
     );
   }
 }
